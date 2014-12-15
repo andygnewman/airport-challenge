7f6d04082ea51d31c7cf982e0e0043ee4261579e
Andy <andygnewman@gmail.com>
2014-12-15 11:13:52 +0000

    substituted douple for plane in airport spec

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,25 +6,23 @@ describe Airport do
   before {allow(airport_sunny).to receive(:weather_condition).and_return(:sunny)}
   let(:airport_stormy) {Airport.new}
   before {allow(airport_stormy).to receive(:weather_condition).and_return(:stormy)}
-  let(:plane) {Plane.new}
+  let(:plane_dble) {double :plane_dble, fly!: true, land!: false}
   let(:airspace) {Airspace.new}
 
   context 'taking off and landing' do
 
     it 'can land a plane' do
-      airspace.accept(plane)
-      airport_sunny.land(plane, airspace)
+      airspace.accept(plane_dble)
+      airport_sunny.land(plane_dble, airspace)
       expect(airport_sunny.number_planes).to eq(1)
-      expect(plane).not_to be_flying
       expect(airspace.number_planes).to eq(0)
     end
 
     it 'can launch a plane (take off)' do
-      plane.land!
-      airport_sunny.accept(plane)
-      airport_sunny.launch(plane, airspace)
+      plane_dble.land!
+      airport_sunny.accept(plane_dble)
+      airport_sunny.launch(plane_dble, airspace)
       expect(airspace.number_planes).to eq(1)
-      expect(plane).to be_flying
       expect(airport_sunny.number_planes).to eq(0)
     end
 
@@ -33,8 +31,8 @@ describe Airport do
   context 'traffic control' do
 
     it 'a plane cannot land if the airport is full' do
-      6.times{ airport_sunny.accept(Plane.new) }
-      expect(lambda {airport_sunny.land(plane, airspace)}).to raise_error(RuntimeError, 'Plane cannot land: Airport Full')
+      6.times{ airport_sunny.accept(plane_dble) }
+      expect(lambda {airport_sunny.land(plane_dble, airspace)}).to raise_error(RuntimeError, 'Plane cannot land: Airport Full')
     end
 
   end
@@ -42,16 +40,16 @@ describe Airport do
   context 'weather conditions' do
 
     it 'should not allow a plane to take off when there is stormy weather' do
-      plane.land!
-      airport_stormy.accept(plane)
+      plane_dble.land!
+      airport_stormy.accept(plane_dble)
       expect(airport_stormy.number_planes).to eq(1)
-      expect(lambda {airport_stormy.launch(plane, airspace)}).to raise_error(RuntimeError, 'Cannot complete action: Stormy Weather')
+      expect(lambda {airport_stormy.launch(plane_dble, airspace)}).to raise_error(RuntimeError, 'Cannot complete action: Stormy Weather')
       expect(airport_stormy.number_planes).to eq(1)
     end
 
     it 'should not allow a plane to land when there is stormy weather' do
-      airspace.accept(plane)
-      expect(lambda {airport_stormy.land(plane, airspace)}).to raise_error(RuntimeError, 'Cannot complete action: Stormy Weather')
+      airspace.accept(plane_dble)
+      expect(lambda {airport_stormy.land(plane_dble, airspace)}).to raise_error(RuntimeError, 'Cannot complete action: Stormy Weather')
       expect(airspace.number_planes).to eq(1)
       expect(airport_stormy.number_planes).to eq (0)
     end
