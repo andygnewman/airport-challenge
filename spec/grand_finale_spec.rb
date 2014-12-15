d44b1ca81a6f7e2bf1768c577faadf016d4e5263
Andy <andygnewman@gmail.com>
2014-12-15 11:33:06 +0000

    add test condition for airport & airspace number to grand finale spec

--- spec/grand_finale_spec.rb
+++ spec/grand_finale_spec.rb
@@ -6,7 +6,6 @@ describe "The grand finale (last spec)" do
 
   let(:sunny_airport) {Airport.new}
   before {allow(sunny_airport).to receive(:weather_condition).and_return(:sunny)}
-  let(:plane) {Plane.new}
   let(:airspace) {Airspace.new}
 
   it 'should allow all planes to land and all planes to take off' do
@@ -14,12 +13,14 @@ describe "The grand finale (last spec)" do
     while airspace.number_planes > 0 do
       sunny_airport.land(airspace.planes[0], airspace)
     end
+    expect(sunny_airport.number_planes).to eq(6)
     sunny_airport.planes.each do | plane |
       expect(plane).not_to be_flying
     end
     while sunny_airport.number_planes > 0 do
       sunny_airport.launch(sunny_airport.planes[0], airspace)
     end 
+    expect(airspace.number_planes).to eq(6)
     airspace.planes.each do | plane |
       expect(plane).to be_flying
     end
